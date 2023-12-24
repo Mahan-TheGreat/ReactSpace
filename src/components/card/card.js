f19090db4421865ac1813fa2d704491d04beadf4
MahanTimalsena <C0902945@MYLAMBTON.CA>
2023-12-23 23:31:31 -0500

    Added Image to card Component

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./card.css";
 import PrimaryButton from "../button/button";
+import CardImage from "../../logo.svg";
 
 function Card() {
   return (
     <>
       <div className="def-card card">
+        <img class="card-img-top" src={CardImage} alt="" />
         <h2 className="">Title </h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
