8f65f3a62c7fb13cb89a21443061a658bf9f2313
MahanTimalsena <C0902945@MYLAMBTON.CA>
2024-03-01 22:40:17 -0500

    Modified Card Component to take custom class name as props

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,13 +1,24 @@
 import PrimaryButton from "../button/button";
 import "./card.css";
 
-function Card({ imageSource, title, text }) {
+function Card({
+  imageSource,
+  title,
+  text,
+  imageClass,
+  titleClass,
+  textClass,
+  buttonClass,
+}) {
   return (
-    <div className="def-card card">
-      <img src={imageSource} alt="card" />
-      <h5>{title}</h5>
-      <p>{text}</p>
-      <PrimaryButton innerText="READ MORE"></PrimaryButton>
+    <div className="card">
+      <img src={imageSource} className={imageClass} alt="card" />
+      <h5 className={titleClass}>{title}</h5>
+      <p className={textClass}>{text}</p>
+      <PrimaryButton
+        innerText="READ MORE"
+        className={buttonClass}
+      ></PrimaryButton>
     </div>
   );
 }
